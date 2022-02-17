c06f70935deb074ee87be7c5454972c04974e9d3
gui42 <guilherme.hermes@pm.com>
2022-02-16 23:07:08 -0300

    making the options better

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ import click
 
 @click.command()
 @click.option("--name", "--n", help="Your name", default="user", type=str)
-@click.option("--hour", "--h", help="End of the day", default=18, type=int)
+@click.option(
+    "--hour",
+    "--h",
+    help="how long to this hour",
+    default=18,
+    type=click.IntRange(0, 23, clamp=True),
+)
 @click.option("--fast", "--f", "-f", is_flag=True)
-@click.option("--minutes", "--m", help="", default=00, type=int)
+@click.option(
+    "--minutes",
+    "--m",
+    help="how low to this precise momment",
+    default=00,
+    type=click.IntRange(0, 59, clamp=True),
+)
 @click.option(
     "--shout/--no-shout", help="everything gets upper case!", default=False, type=bool
 )
 def tempo(name, hour, minutes, fast, shout):
-    """Does some stupid shit"""
+    """Calculates how long before a momment, Defaults to how long till 6'clock"""
     error_message = click.style("cannot", fg="red", underline=True)
-    if hour > 23:
-        click.echo(f"hour {error_message} be greater than 23")
-        return
-    if minutes > 59:
-        click.echo(f"minutes {error_message} be greater than 59")
-        return
     if name == "user":
         name = get_username()
 
@@ -37,9 +43,6 @@ def tempo(name, hour, minutes, fast, shout):
         confirmation = True
 
     if confirmation:
-        #time_now = "{0}:{1:0>2}".format(now.hour, now.minute)
-        #time_now_format = click.style(time_now, fg="red", bold=True)
-        #click.echo(time_now_format)
         then = now.replace(hour=hour, minute=minutes)
         if now > then:
             extra_time = now - then
